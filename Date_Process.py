4120935354b041aaabceb0a41acb9281d43cd26e
Ro <r.f.peake@gmail.com>
2018-04-07 18:11:03 +0100

    Concatenate

--- Date_Process.py
+++ Date_Process.py
@@ -8,11 +8,22 @@ Created on Sat Jan 20 14:18:07 2018
 import pandas as pd
 import win32ui
 import win32con
+import os.path
 o=win32ui.CreateFileDialog(1)
 if o.DoModal()==1:
-    filename=o.GetPathName()
+    selected_path=o.GetPathName()
+    #renamed from filename for clarity
+    
 #%%
-    df=pd.read_csv(filename,skiprows=5,header=0,
+    folder_name=os.path.dirname(selected_path)
+    list_of_files=os.listdir(folder_name)
+    selected_file=os.path.basename(selected_path)    
+    sf_index=list_of_files.index(selected_file)
+    previous_file=list_of_files[sf_index-1]
+    previous_path=os.path.join(folder_name,previous_file)
+#%%
+def open_and_column_fix(csv_path):
+    df=pd.read_csv(csv_path,skiprows=5,header=0,
                    names=['transaction_date','posted_date','type','description','b1','credit','debit','balance','b2','b3'],
                    )
     df.transaction_date=df.description.str.split(' ON ',expand=True)[1]
@@ -20,10 +31,19 @@ if o.DoModal()==1:
     df['sort_code']="'09-01-28"
     df['ac_number']=95349265
     df=df[['transaction_date','posted_date','sort_code','ac_number','type','description','credit','debit','balance']]
-    df.to_clipboard(excel=True,index=False,header=False)
-#%%   
+    return df
 
-#here's my new code to find duplicates but it's shit and broken
+#   df.to_clipboard(excel=True,index=False,header=False)
+#here's my new code to find duplicates but it's shit and broken    
+    
+#%%   
+    df_now=open_and_column_fix(selected_path)
+    df_old=open_and_column_fix(previous_path)
+    df=pd.concat([df_now, df_old])
+    df=df.drop_duplicates(keep='first')
+    len(df)
+    
+#%%
     win32ui.MessageBox('Success! Paste away!','Transaction Formatter 2000')
 else:
-    win32ui.MessageBox('Well ok then, bye I guess','Transaction Formatter 2000',win32con.MB_ICONSTOP)
+    win32ui.MessageBox('Bye bye','Transaction Formatter 2000',win32con.MB_ICONSTOP)
